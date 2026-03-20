97b584ba5a253218c0168d2d9a59ea996d59ea51
unknown <agent.rabbit.txs@gmail.com>
2026-03-20 15:54:53 -0300

    \feat: add weather dashboard project\

--- dist/weather/dto/weather.dto.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class WeatherResponseDto {
-    temperature: number;
-    humidity: number;
-    description: string;
-    city: string;
-}
--- dist/weather/weather.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { WeatherResponseDto } from './dto/weather.dto';
-export declare class WeatherService {
-    private readonly logger;
-    private readonly mockCities;
-    getCurrentWeather(): Promise<WeatherResponseDto>;
-}
